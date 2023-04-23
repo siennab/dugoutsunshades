9a27b0ab264a2d1493523c133bae79829c473d50
siennab <siennabast@gmail.com>
2023-04-23 14:58:13 -0500

    docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: ['./src/js/index.js'],
   output: {
     filename: 'main.js',
-   // path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     publicPath: '/',
   },
   module: {
