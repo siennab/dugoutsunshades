507eca321eb8f38f7e40ec7001553adbea19e2c1
siennab <siennabast@gmail.com>
2023-04-23 15:01:37 -0500

    publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'docs'),
-    publicPath: '/',
+    publicPath: '/dugoutsunshades',
   },
   module: {
     rules: [
