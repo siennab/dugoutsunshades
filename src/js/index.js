e56452828e8acf7514f22ded7fdd020f59289b86
siennab <siennabast@gmail.com>
2023-04-27 20:47:41 -0500

    Images / ui updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 import '../css/main.scss';
+import Siema from 'siema';
+
+
+window.addEventListener('load', (e) => {
+    const siema = new Siema({
+        selector: '.siema',
+        duration: 300,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: true,
+        rtl: false,
+        onInit: () => {},
+        onChange: () => {},
+      });
+
+    setInterval(() => {
+        siema.next();
+    }, 3500);
+});
